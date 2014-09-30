19394ea9f8d2447f9fe650d6f30478f0da602553
Luca Rebuffi <luca.rebuffi@elettra.eu>
2014-09-30 14:21:37 +0200

    Shadow as an add-on. Restored original shape of Origin (with customizations)
    30/09/2014

--- Orange/widgets/__init__.py
+++ Orange/widgets/__init__.py
@@ -8,16 +8,9 @@ import pkg_resources
 def widget_discovery(discovery):
     #from . import data
     dist = pkg_resources.get_distribution("Orange")
-    pkgs = [#"Orange.widgets.data",
-            #"Orange.widgets.visualize",
-            #"Orange.widgets.classify",
-            #"Orange.widgets.evaluate",
-            "Orange.widgets.shadow_experimental_elements",
-            "Orange.widgets.shadow_optical_elements",
-            "Orange.widgets.shadow_preprocessor",
-            "Orange.widgets.shadow_user_defined",
-            "Orange.widgets.shadow_loop_management",
-            "Orange.widgets.shadow_sources",
-            "Orange.widgets.shadow_plots"]
+    pkgs = ["Orange.widgets.data",
+            "Orange.widgets.visualize",
+            "Orange.widgets.classify",
+            "Orange.widgets.evaluate"]
     for pkg in pkgs:
         discovery.process_category_package(pkg, distribution=dist)
