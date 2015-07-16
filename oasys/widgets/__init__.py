1f856f6fd9d3b9e390f1f3c5b41ff62741fe2cec
Ales Erjavec <ales.erjavec@fri.uni-lj.si>
2015-07-16 16:10:28 +0200

    Remove copies of orange widget modules.

--- oasys/widgets/__init__.py
+++ oasys/widgets/__init__.py
@@ -1,18 +1,6 @@
 """
-
 """
-import pkg_resources
+import Orange.widgets
 
+__path__.extend(Orange.widgets.__path__)
 
-# Entry point for main Orange categories/widgets discovery
-def widget_discovery(discovery):
-    #from . import data
-    dist = pkg_resources.get_distribution("Orange")
-    pkgs = ["Orange.widgets.data",
-            "Orange.widgets.visualize",
-            "Orange.widgets.classify",
-            "Orange.widgets.regression",
-            "Orange.widgets.evaluate",
-            "Orange.widgets.unsupervised"]
-    for pkg in pkgs:
-        discovery.process_category_package(pkg, distribution=dist)
