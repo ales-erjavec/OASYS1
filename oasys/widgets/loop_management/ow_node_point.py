70f00e50adaf8b6fd798f401a154599af1746c35
lucarebuffi <lrebuffi@anl.gov>
2019-01-04 09:16:45 -0600

    Loopers, suspend/restart

--- oasys/widgets/loop_management/ow_node_point.py
+++ oasys/widgets/loop_management/ow_node_point.py
@@ -6,7 +6,7 @@ from oasys.widgets import gui as oasysgui
 from oasys.widgets.gui import ConfirmDialog
 
 from orangewidget import gui
-from PyQt5 import QtGui
+from PyQt5.QtGui import QFont, QPalette, QColor
 from orangewidget.settings import Setting
 
 from oasys.util.oasys_util import TriggerIn, TriggerOut
@@ -33,36 +33,62 @@ class LoopPoint(widget.OWWidget):
     number_of_new_objects = Setting(1)
     current_new_object = 0
     run_loop = True
+    suspend_loop = False
 
     #################################
     process_last = True
     #################################
 
     def __init__(self):
-        self.runaction = OWAction("Start Loop", self)
+        self.runaction = OWAction("Start", self)
         self.runaction.triggered.connect(self.startLoop)
         self.addAction(self.runaction)
 
-        self.runaction = OWAction("Interrupt", self)
+        self.runaction = OWAction("Stop", self)
         self.runaction.triggered.connect(self.stopLoop)
         self.addAction(self.runaction)
 
+        self.runaction = OWAction("Suspend", self)
+        self.runaction.triggered.connect(self.suspendLoop)
+        self.addAction(self.runaction)
+
+        self.runaction = OWAction("Restart", self)
+        self.runaction.triggered.connect(self.restartLoop)
+        self.addAction(self.runaction)
+
         self.setFixedWidth(400)
-        self.setFixedHeight(185)
+        self.setFixedHeight(220)
 
         button_box = oasysgui.widgetBox(self.controlArea, "", addSpace=True, orientation="horizontal")
 
-        self.start_button = gui.button(button_box, self, "Start Loop", callback=self.startLoop)
-        self.start_button.setFixedHeight(45)
+        self.start_button = gui.button(button_box, self, "Start", callback=self.startLoop)
+        self.start_button.setFixedHeight(35)
 
-        stop_button = gui.button(button_box, self, "Interrupt", callback=self.stopLoop)
-        stop_button.setFixedHeight(45)
-        font = QtGui.QFont(stop_button.font())
+        stop_button = gui.button(button_box, self, "Stop", callback=self.stopLoop)
+        stop_button.setFixedHeight(35)
+        font = QFont(stop_button.font())
         font.setBold(True)
         stop_button.setFont(font)
-        palette = QtGui.QPalette(stop_button.palette()) # make a copy of the palette
-        palette.setColor(QtGui.QPalette.ButtonText, QtGui.QColor('red'))
+        palette = QPalette(stop_button.palette()) # make a copy of the palette
+        palette.setColor(QPalette.ButtonText, QColor('red'))
         stop_button.setPalette(palette) # assign new palette
+
+        self.stop_button = stop_button
+
+        button_box = oasysgui.widgetBox(self.controlArea, "", addSpace=True, orientation="horizontal")
+
+        suspend_button = gui.button(button_box, self, "Suspend", callback=self.suspendLoop)
+        suspend_button.setFixedHeight(35)
+        font = QFont(suspend_button.font())
+        font.setBold(True)
+        suspend_button.setFont(font)
+        palette = QPalette(suspend_button.palette()) # make a copy of the palette
+        palette.setColor(QPalette.ButtonText, QColor('orange'))
+        suspend_button.setPalette(palette) # assign new palette
+
+        self.re_start_button = gui.button(button_box, self, "Restart", callback=self.restartLoop)
+        self.re_start_button.setFixedHeight(35)
+        self.re_start_button.setEnabled(False)
 
         left_box_1 = oasysgui.widgetBox(self.controlArea, "Loop Management", addSpace=True, orientation="vertical", width=380, height=100)
 
@@ -70,14 +96,13 @@ class LoopPoint(widget.OWWidget):
 
         self.le_current_new_object = oasysgui.lineEdit(left_box_1, self, "current_new_object", "Current New " + self.get_object_name(), labelWidth=250, valueType=int, orientation="horizontal")
         self.le_current_new_object.setReadOnly(True)
-        font = QtGui.QFont(self.le_current_new_object.font())
+        font = QFont(self.le_current_new_object.font())
         font.setBold(True)
         self.le_current_new_object.setFont(font)
-        palette = QtGui.QPalette(self.le_current_new_object.palette()) # make a copy of the palette
-        palette.setColor(QtGui.QPalette.Text, QtGui.QColor('dark blue'))
-        palette.setColor(QtGui.QPalette.Base, QtGui.QColor(243, 240, 160))
+        palette = QPalette(self.le_current_new_object.palette()) # make a copy of the palette
+        palette.setColor(QPalette.Text, QColor('dark blue'))
+        palette.setColor(QPalette.Base, QColor(243, 240, 160))
         self.le_current_new_object.setPalette(palette)
-
 
         gui.rubber(self.controlArea)
 
@@ -91,7 +116,27 @@ class LoopPoint(widget.OWWidget):
         if ConfirmDialog.confirmed(parent=self, message="Confirm Interruption of the Loop?"):
             self.run_loop = False
             self.setStatusMessage("Interrupted by user")
-            #self.warning("Interrupted by user")
+
+    def suspendLoop(self):
+        try:
+            if ConfirmDialog.confirmed(parent=self, message="Confirm Suspension of the Loop?"):
+                self.run_loop = False
+                self.suspend_loop = True
+                self.stop_button.setEnabled(False)
+                self.re_start_button.setEnabled(True)
+                self.setStatusMessage("Suspended by user")
+        except:
+            pass
+
+    def restartLoop(self):
+        try:
+            self.run_loop = True
+            self.suspend_loop = False
+            self.stop_button.setEnabled(True)
+            self.re_start_button.setEnabled(False)
+            self.passTrigger(TriggerIn(new_object=True))
+        except:
+            pass
 
     def passTrigger(self, trigger):
         if self.run_loop:
@@ -113,18 +158,22 @@ class LoopPoint(widget.OWWidget):
                         self.setStatusMessage("")
                         self.send("Trigger", TriggerOut(new_object=False))
         else:
-            self.current_new_object = 0
-            self.start_button.setEnabled(True)
+            if not self.suspend_loop:
+                self.current_new_object = 0
+                self.start_button.setEnabled(True)
+
             self.send("Trigger", TriggerOut(new_object=False))
-            #self.warning()
             self.setStatusMessage("")
             self.run_loop = True
+            self.suspend_loop = False
 
     def get_object_name(self):
         return "Object"
 
+from PyQt5.QtWidgets import QApplication
+
 if __name__ == "__main__":
-    a = QtGui.QApplication(sys.argv)
+    a = QApplication(sys.argv)
     ow = LoopPoint()
     ow.show()
     a.exec_()
